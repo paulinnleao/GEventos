d88a5ced10890319665821c502bf3a97f5bdb366
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 20:06:54 -0300

    Implement: Swagger in RestNotification

--- src/main/java/com/paulinnleao/geventos/notification/rest/RestNotification.java
+++ src/main/java/com/paulinnleao/geventos/notification/rest/RestNotification.java
@@ -1,5 +1,11 @@
 package com.paulinnleao.geventos.notification.rest;
 
+import com.paulinnleao.geventos.notification.Notification;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +15,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/notifications")
+@Tag(name = "Notification", description = "Endpoints to manage notifications")
 public interface RestNotification {
 
     @GetMapping
+    @Operation(
+            summary = "Get a notification",
+            description = "Get a notification if exits with UUID",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Notification.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     ResponseEntity<List<?>> allNotification();
 }
