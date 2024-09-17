d7e5cc8d12105752c2059a2035c001b1ab85f907
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 08:15:38 -0300

    Initializing Project

---
+++ src/main/java/com/paulinnleao/geventos/GeventosApplication.java
@@ -0,0 +1,13 @@
+package com.paulinnleao.geventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulinnleao/geventos/GeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulinnleao.geventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
