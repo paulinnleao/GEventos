30be6b62a92df2fd2e07f272f202cc2f99f2e220
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 20:04:24 -0300

    Implement: Swagger in RestUser

--- src/main/java/com/paulinnleao/geventos/user/rest/RestUser.java
+++ src/main/java/com/paulinnleao/geventos/user/rest/RestUser.java
@@ -1,6 +1,11 @@
 package com.paulinnleao.geventos.user.rest;
 
 import com.paulinnleao.geventos.user.UserRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,17 +13,66 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User", description = "Endpoints to manage users")
 public interface RestUser {
     @GetMapping
+    @Operation(
+            summary = "Get all users",
+            description = "Get all users if exits pageable",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     ResponseEntity<List<?>> getAllUsers();
 
     @GetMapping(value = "/{idUser}")
+    @Operation(
+            summary = "Get an users",
+            description = "Get an users if exits using the UUID",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     ResponseEntity<?> getUserById(@PathVariable(value = "idUser") Integer idUser);
 
     @PutMapping(value = "/{idUser}")
+    @Operation(
+            summary = "Update an user if exists",
+            description = "Update an user if exists with his UUID. Get the user with UUID and update with body data",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     ResponseEntity<?> updateUserById(@PathVariable(value = "idUser") Integer idUser,
                                             @RequestBody UserRequestDTO userRequestDTO
                                             );
     @DeleteMapping(value = "/{idUser}")
+    @Operation(
+            summary = "Delete an user",
+            description = "Delete an user with UUID if exists",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     ResponseEntity<?> deleteUserById(@PathVariable(value = "idUser") Integer idUser);
 }
