34fbc0234e46dfbee353e8911b2626926f30b123
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 08:50:19 -0300

    Authentication Creation Rest Controller

---
+++ src/main/java/com/paulinnleao/geventos/user/UserReciveDTO.java
@@ -0,0 +1,4 @@
+package com.paulinnleao.geventos.user;
+
+public record UserReciveDTO(String name, String email, String password, String role) {
+}
---
+++ src/main/java/com/paulinnleao/geventos/user/rest/RestAuthentication.java
@@ -0,0 +1,23 @@
+package com.paulinnleao.geventos.user.rest;
+
+import com.paulinnleao.geventos.user.UserReciveDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class RestAuthentication {
+
+    @PostMapping("/register")
+    ResponseEntity<?> register(@RequestBody UserReciveDTO userReciveDTO){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("login")
+    ResponseEntity<?> login(@RequestBody UserReciveDTO userReciveDTO){
+        return ResponseEntity.ok().build();
+    }
+}
