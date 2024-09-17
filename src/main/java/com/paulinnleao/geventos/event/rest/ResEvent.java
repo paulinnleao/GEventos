31477a9df49c98e4b898a049df1cdc8f3862c94f
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 12:40:02 -0300

    Register Event Creation Rest Controller

--- src/main/java/com/paulinnleao/geventos/event/rest/ResEvent.java
+++ src/main/java/com/paulinnleao/geventos/event/rest/ResEvent.java
@@ -1,5 +1,6 @@
 package com.paulinnleao.geventos.event.rest;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +25,25 @@ public class ResEvent {
     }
 
     @PutMapping(name = "/{idEvent}")
-    public ResponseEntity<?> updateEventById(@PathVariable(name = "idEvent")Integer idEvenet){
+    public ResponseEntity<?> updateEventById(@PathVariable(name = "idEvent")Integer idEvent){
         return null;
     }
 
     @DeleteMapping(name = "{idEvent}")
-    public ResponseEntity<?> deleteEventById(@PathVariable(name = "idEvent") Integer idEvenet){
+    public ResponseEntity<?> deleteEventById(@PathVariable(name = "idEvent") Integer idEvent){
         return ResponseEntity.noContent().build();
     }
+
+
+    // Registers
+    @PostMapping(name = "/{idEvent}/enroll")
+    public ResponseEntity<?> registerAtTheEvent(@PathVariable(value = "idEvent") Integer idEvent){
+        return null;
+    }
+
+    @DeleteMapping(name = "/{idEvent}/enroll")
+    public ResponseEntity<?> deleteRegisterById(@PathVariable(name = "idEvent") Integer idEvent){
+        return ResponseEntity.noContent().build();
+    }
+
 }
