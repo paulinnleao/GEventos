4a4f08decc9a92a3da34663ef71d98808f334c3e
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-09-17 17:01:49 -0300

    Implement: Swagger in RestAuthentication

--- src/main/java/com/paulinnleao/geventos/user/rest/RestAuthentication.java
+++ src/main/java/com/paulinnleao/geventos/user/rest/RestAuthentication.java
@@ -1,6 +1,11 @@
 package com.paulinnleao.geventos.user.rest;
 
 import com.paulinnleao.geventos.user.UserRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +14,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "Endpoints to manage authentication")
 public class RestAuthentication {
 
     @PostMapping("/register")
+    @Operation(
+            summary = "Register a new user",
+            description = "Register a new user if not exists",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> register(@RequestBody UserRequestDTO userRequestDTO){
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Singin up",
+            description = "If an account exist, this will allow to log in",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserRequestDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<?> login(@RequestBody UserRequestDTO userRequestDTO){
         return ResponseEntity.ok().build();
     }
